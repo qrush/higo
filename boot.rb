8bc992524e2458e2adba845dd191d444f65af527
Nick Quaranto <nick@thoughtbot.com>
2011-06-15 23:10:24 -0400

    chmod maybe

--- boot.rb
+++ boot.rb
@@ -1,8 +1,10 @@
 pwd  = `pwd`.strip
+chmod = `chmod -v 755 higo`
+print chmod
 dir = `ls -lah`
 print dir
-port = ARGV[0]
 
+port = ARGV[0]
 puts "starting this up!"
 puts "pwd: #{pwd}"
 puts "port: #{port}"
