e052c2128b95719df6332476a6fd6f85cbe8e04c
Nick Quaranto <nick@thoughtbot.com>
2011-06-16 00:10:05 -0400

    add some helper text

--- higo.go
+++ higo.go
@@ -8,7 +8,7 @@ import (
 var port *string = flag.String("port", "3000", "server port")
 
 func Hello(env mango.Env) (mango.Status, mango.Headers, mango.Body) {
-	return 200, mango.Headers{}, mango.Body("Hi Go!")
+	return 200, mango.Headers{}, mango.Body("Hi from <a href='http://golang.org'>Go!</a><br />This is an experiment to get Go working on Heroku by <a href='http://twitter.com/qrush'>@qrush</a>.<br />Source is up on <a href='http://github.com/qrush/higo'>GitHub</a> for now until I get a blog post out.")
 }
 
 func main() {
